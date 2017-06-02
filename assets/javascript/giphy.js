eb88aa9a59456cb85dcd2d12f48d4125d9db2574
Praveen Peruri <praveenperuri@Praveens-MBP.fios-router.home>
2017-06-01 21:48:09 -0500

    final changes

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,7 +1,7 @@
  // Initial array of topics
  var topics = ["Lebron James", "Odell Beckham Jr.", "Cristiano Ronaldo", "Tom Brady", "LeVeon Bell", "JJ Watt", "Von Miller", "Michael Phelps", "Roger Federer"];
 
- // displaytopicInfo function re-renders the HTML to display the appropriate content
+ // display topic Info function re-renders the HTML to display the appropriate content
  function displayInfo() {
 
      var topic = $(this).attr("data-name");
@@ -13,7 +13,6 @@
          method: "GET"
      }).done(function(response) {
 
-         // YOUR CODE GOES HERE!!!
          console.log(response);
 
          var myJSON = JSON.stringify(response.data);
@@ -31,10 +30,6 @@
          topicInfo.append(head);
 
          for (i = 0; i < response.data.length; i++) {
-
-             // if (response.data[i].user == null || response.data[i].user.length == 0) {
-             //     continue;
-             // }
 
              var gifDiv = $("<div>");
 
@@ -56,9 +51,7 @@
 
              poster.attr("title", "Click Me");
 
-             //poster.attr("still-src", response.data[i].user.avatar_url);
-
-             poster.attr("still-src", response.data[i].images.downsized_still.url);
+             //poster.attr("still-src", response.data[i].images.downsized_still.url);
 
              poster.attr("anim-src", response.data[i].images.downsized.url);
 
@@ -71,9 +64,6 @@
              picCounter++;
 
 
-             // if (picCounter == 10) {
-             //     break;
-             // }
 
          }
 
@@ -81,42 +71,36 @@
 
  }
 
- // Function for displaying topic data
+
  function renderButtons() {
 
-     // Deletes the topics prior to adding new topics
-     // (this is necessary otherwise you will have repeat buttons)
      $("#buttons-view").empty();
 
-     // Loops through the array of topics
      for (var i = 0; i < topics.length; i++) {
 
-         // Then dynamicaly generates buttons for each topic in the array
-         // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
          var a = $("<button>");
-         // Adds a class of topic to our button
+
          a.addClass("topic");
-         // Added a data-attribute
+
          a.attr("data-name", topics[i]);
-         // Provided the initial button text
+
          a.text(topics[i]);
-         // Added the button to the buttons-view div
+
          $("#buttons-view").append(a);
      }
  }
 
- // This function handles events where the add topic button is clicked
  $("#add-topic").on("click", function(event) {
      event.preventDefault();
-     // This line of code will grab the input from the textbox
+
      var topic = $("#topic-input").val().trim();
 
-     // The topic from the textbox is then added to our array
+
      topics.push(topic);
 
      $("#topic-input").val('');
 
-     // Calling renderButtons which handles the processing of our topic array
+
      renderButtons();
 
  });
@@ -133,7 +117,6 @@
  }
 
 
- // Adding click event listeners to all elements with a class of "topic"
  $(document).on("click", ".topic", displayInfo);
 
  $(document).on("click", ".image", animateGif);
